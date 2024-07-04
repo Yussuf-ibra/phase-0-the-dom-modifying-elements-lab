29c70df328d43901914d57f18496128e0007046d
yussuf <yussuf.ibrahim@student.moringaschool.com>
2024-07-04 20:30:16 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.querySelector('main#main').remove();
 const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.innerHTML = 'YOUR-NAME is the champion';
+newHeader.innerHTML = 'YUSSUF is the champion';
 document.body.append(newHeader);
